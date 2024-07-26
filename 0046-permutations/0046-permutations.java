6f3c1adf89cf81633f30c5ba53e8f1b7f57ea276
Threshika V S <115970544+Threshik@users.noreply.github.com>
2024-07-27 01:16:36 +0530

    Time: 1 ms (94.02%), Space: 44.7 MB (15.88%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,28 @@
+class Solution {
+    static void find(List<List<Integer>> ans, List<Integer> ds,boolean[] vis,int[] nums)
+    {
+        if(ds.size()==nums.length)
+        {
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!vis[i])
+            {
+                vis[i]=true;
+                ds.add(nums[i]);
+                find(ans,ds,vis,nums);
+                ds.remove(ds.size()-1);
+                vis[i]=false;
+            }
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        boolean[] vis = new boolean[nums.length];
+        find(ans,ds,vis,nums);
+        return ans;
+    }
+}
